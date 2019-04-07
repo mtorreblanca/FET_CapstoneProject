f4c4eb1aacde8d6844201c2384fe582ca9b2f13e
Marcelo <m_torreblanca@outlook.com>
2019-04-07 12:09:49 -0500

    carousel manual done

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -7,7 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() {}
+  slideIndex = 1;
 
-  ngOnInit() {}
+  constructor() { }
+
+  ngOnInit() {
+    this.showSlides(this.slideIndex);
+  }
+
+  plusSlides(n) {
+    this.showSlides(this.slideIndex += n);
+    console.log(this.slideIndex);
+  }
+
+  currentSlide(n) {
+    this.showSlides(this.slideIndex = n);
+  }
+
+  showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName('mySlides') as HTMLCollectionOf<HTMLElement>;
+    const dots = document.getElementsByClassName('dot');
+
+    if (n < 1) { this.slideIndex = slides.length; }
+    if (n > 3) { this.slideIndex = 1; }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(' active', '');
+    }
+    slides[this.slideIndex - 1].style.display = 'block';
+    dots[this.slideIndex - 1].className += ' active';
+  }
 }
