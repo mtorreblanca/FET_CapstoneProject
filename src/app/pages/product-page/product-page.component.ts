3e29c0d32901e09ce1eedbdc1dd9bcc72f8ebfad
Marcelo <m_torreblanca@outlook.com>
2019-04-26 09:26:32 -0500

    ProductPageComponent added logic for retrieving items and getting name of item from Home

--- src/app/pages/product-page/product-page.component.ts
+++ src/app/pages/product-page/product-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { ShoppingDataService } from 'src/app/services/shopping-data.service';
 
 @Component({
   selector: 'app-product-page',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent implements OnInit {
-
-  constructor() { }
+  items$: any;
+  constructor(private route: ActivatedRoute,
+              private data: ShoppingDataService,
+              private location: Location
+              ) { }
 
   ngOnInit() {
+    this.getItem();
+  }
+
+  getItem() {
+    const id = this.route.snapshot.paramMap.get('id');
+    console.log(id);
+
+    this.data.getItems()
+      .subscribe(items => {
+        this.items$ = items;
+        console.log(this.items$);
+      });
   }
 
 }
