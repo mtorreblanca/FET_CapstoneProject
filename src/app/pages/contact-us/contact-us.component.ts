d982cc9431493b4d8fc1d3bd4448e801038f79a5
Marcelo <m_torreblanca@outlook.com>
2019-04-29 01:11:21 -0500

    contact us page done

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsComponent implements OnInit {
 
+  formValid: any = true;
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit(form) {
+    this.formValid = form.valid;
+    console.log(form.value);
+    if (form.valid) {
+      // rubric60 The send button should create an alert based on the message sent
+      alert('Your message details:\n'
+        + form.value.name + '\n'
+        + form.value.email + '\n'
+        + form.value.subject + '\n'
+        + form.value.message + '\n');
+    }
+  }
 }
