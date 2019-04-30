fec36b9e00c87ebbc1d0d215a817b157bb1403fb
Marcelo <m_torreblanca@outlook.com>
2019-04-29 23:00:30 -0500

    All working till know, just Shopping Page Stock and Sorting left and About Us Page left

--- src/app/pages/shopping-page/shopping-page.component.ts
+++ src/app/pages/shopping-page/shopping-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingDataService } from 'src/app/services/shopping-data.service';
 
 @Component({
   selector: 'app-shopping-page',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingPageComponent implements OnInit {
 
-  constructor() { }
+  itemCategory = 'Baby Care';
+  itemsFound: any;
+  itemsTotal: any;
+  itesmStock: any;
+  forStock: false;
+  constructor(private data: ShoppingDataService) { }
 
   ngOnInit() {
+    this.getItems(this.itemCategory);
   }
 
+
+  getItems(itemCategory) {
+    this.itemCategory = itemCategory;
+    this.data.getItems()
+      .subscribe(items => {
+        items.forEach(res => {
+          const find = res.subcategories.find(t => t.name === itemCategory);
+          if (find) {
+            this.itemsFound = find;
+            this.itemsTotal = this.itemsFound.items.length;
+          }
+        });
+      });
+  }
+  // rubric30 Clicking on the “Add” button inside a grid cell should add 1 unit of the associated product to the shopping cart
+  addToCart(item) {
+    const newCartItem = {
+      name: item.name,
+      details: item.description,
+      price: item.price,
+      quantity: 1,
+      imagelink: item.imagelink
+    };
+    this.data.addItem(newCartItem);
+  }
+
+  onStock() {
+    if (this.forStock === false) {
+      console.log('hi im false');
+    } else {
+      console.log('hi! im true!');
+    }
+  }
 }
